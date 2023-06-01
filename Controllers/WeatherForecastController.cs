6c50eb1f655d09623a6059fb3ef17f436159c59e
castanedaLuis <castanedaLuis@github.com>
2023-05-31 19:05:09 -0600

    Implementando los metodos POST, DELETE

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -12,21 +12,46 @@ public class WeatherForecastController : ControllerBase
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
+    private static List<WeatherForecast> ListWeatherForecast = new List<WeatherForecast>();
 
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
         _logger = logger;
+        if(ListWeatherForecast == null || !ListWeatherForecast.Any())
+        {
+                ListWeatherForecast = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    //Date = DateTime.Now.AddDays(index),
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToList();
+        }
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
+   [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        return ListWeatherForecast;
+    }
+
+    [HttpPost]
+    public IActionResult Post(WeatherForecast weatherForecast)
+    {
+        ListWeatherForecast.Add(weatherForecast);
+        return Ok("Registro insertado con Exito");
+    }
+
+    [HttpDelete("{index}")]
+    public IActionResult Delete(int index)
+    {
+        if(index >= ListWeatherForecast.Count())
         {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+            return NotFound("No se encuenta el elemento");
+        }
+        
+        ListWeatherForecast.RemoveAt(index);
+        return Ok("Registro Eliminado");
     }
 }
